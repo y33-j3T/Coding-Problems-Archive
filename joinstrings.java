880cd07be2c1fc8486cb0bf896731f674f1db219
y33-j3T <yeejet.tech@gmail.com>
2020-02-28 23:13:22 +0800

    optimize joinstrings

--- joinstrings.java
+++ joinstrings.java
@@ -1,37 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class joinstrings {
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); BufferedOutputStream out = new BufferedOutputStream(System.out)) {
-            int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedOutputStream out = new BufferedOutputStream(System.out);
 
-            String[] strArr = new String[n];
+        int n = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < n; i++) {
-                strArr[i] = br.readLine();
-            }
+        String[] strArr = new String[n + 1];
+        int[] idxMap = new int[n + 1];
+        int[] cache = new int[n + 1];
 
-            int idxOut = 0;
-            String input;
-            while ((input = br.readLine()) != null) {
-                if (input.equals("")) break;
-                StringTokenizer st = new StringTokenizer(input);
-                int a = Integer.parseInt(st.nextToken()) - 1;
-                int b = Integer.parseInt(st.nextToken()) - 1;
-
-                strArr[a] += strArr[b];
-                strArr[b] = "";
-                idxOut = a;
-            }
-
-            out.write(strArr[idxOut].getBytes());
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 1; i < n + 1; i++) {
+            strArr[i] = br.readLine();
         }
+
+        int a = 1;
+        int b = 1;
+        int x = a;
+        String[] input;
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if (idxMap[a] == 0) {
+                idxMap[a] = b;
+                cache[a] = b;
+                continue;
+            }
+
+            x = a;
+            while (cache[x] != 0) {
+                x = cache[x];
+            }
+            cache[a] = b;
+            idxMap[x] = b;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (idxMap[a] != 0) {
+            sb.append(strArr[a]);
+            a = idxMap[a];
+        }
+        sb.append(strArr[a]);
+
+        out.write(sb.toString().getBytes());
+
+        br.close();
+        out.close();
     };
 }
