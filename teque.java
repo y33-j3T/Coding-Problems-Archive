c807b809ca76e98df94e73f1a1726a26da7937e9
y33-j3T <yeejet.tech@gmail.com>
2020-03-01 10:49:30 +0800

    optimize teque

--- teque.java
+++ teque.java
@@ -3,9 +3,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedOutputStream;
 import java.util.StringTokenizer;
 import java.io.IOException;
-import java.lang.Math;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashMap;
 
 public class teque {
     public static void main(String[] args) throws Exception {
@@ -13,10 +11,12 @@ public class teque {
         BufferedOutputStream out = new BufferedOutputStream(System.out);
 
         int n = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> teque = new HashMap<>();
-        int head = 0;
-        int mid = 0;
-        int tail = 0;
+        HashMap<Integer, Integer> front = new HashMap<>();
+        HashMap<Integer, Integer> back = new HashMap<>();
+        int frontHead = -1;
+        int frontTail = 0;
+        int backHead = -1;
+        int backTail = 0;
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
@@ -25,16 +25,48 @@ public class teque {
             int x = Integer.parseInt(st.nextToken());
 
             if (command.equals("get")) {
+                if (x < front.size()) {
+                    sb.append(front.get(x + frontHead + 1));
+                } else {
+                    sb.append(back.get(x - front.size() + backHead + 1));
+                }
+                sb.append("\n");
                 continue;
             }
 
             if (command.equals("push_back")) {
-
+                back.put(backTail++, x);
             } else if (command.equals("push_front")) {
-
+                front.put(frontHead--, x);
             } else {
-
+                front.put(frontTail++, x);
             }
+
+            if (front.size() < back.size()) {
+                front.put(frontTail++, back.get(backHead + 1));
+                back.remove(++backHead);
+            } else if (front.size() - 1 > back.size()) {
+                back.put(backHead--, front.get(frontTail - 1));
+                front.remove(--frontTail);
+            }
+
+            // if (!front.isEmpty()){
+            //     StringBuilder frontStr= new StringBuilder("{");
+            //     for (Integer key : front.keySet()) {
+            //         frontStr.append(key + "=" + front.get(key) + ", ");
+            //     }
+            //     frontStr.delete(frontStr.length()-2, frontStr.length()).append("}");
+            //     System.out.println(frontStr);
+            // }
+            //
+            // if (!back.isEmpty()) {
+            //     StringBuilder backStr= new StringBuilder("{");
+            //     for (Integer key : back.keySet()) {
+            //         backStr.append(key + "=" + back.get(key) + ", ");
+            //     }
+            //     backStr.delete(backStr.length()-2, backStr.length()).append("}");
+            //     System.out.println(backStr);
+            // }
         }
 
         out.write(sb.toString().getBytes());
