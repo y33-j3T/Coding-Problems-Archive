982fe9cb5e7f54ad61a310ccf76d39e635964ec3
y33-j3T <yeejet.tech@gmail.com>
2020-04-17 21:45:41 +0800

    done millionairemadness

--- millionairemadness.java
+++ millionairemadness.java
@@ -2,25 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.BufferedOutputStream;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class millionairemadness {
     public static final int[][] DIRS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-    public static final int INF = Integer.MAX_VALUE;
-    public static ArrayList<ArrayList<IntegerPair>> adjList = new ArrayList<>();
-    public static ArrayList<Integer> D = new ArrayList<>();
-    public static ArrayList<Integer> p = new ArrayList<>();
-
-    public static void initSSSP(int n, int s) {
-        D.clear();
-        D.addAll(Collections.nCopies(n, INF));
-        p.clear();
-        p.addAll(Collections.nCopies(n, -1));
-        D.set(s, 0);
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,6 +31,7 @@ public class millionairemadness {
         }
 
         // make adjList for every coordinate in the grid
+        ArrayList<ArrayList<IntegerPair>> adjList = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 ArrayList<IntegerPair> tempList = new ArrayList<>();
@@ -54,61 +44,93 @@ public class millionairemadness {
                     boolean isValidCol = nextCol >= 0 && nextCol < n;
                     if (!isValidRow || !isValidCol) continue;
 
-                    int weight = weightGrid[nextRow][nextCol] - weightGrid[i][j];
-                    if (weight < 0) weight = 0;
+                    int weight = Math.max(0, weightGrid[nextRow][nextCol] - weightGrid[i][j]);
 
-                    tempList.add(new IntegerPair(weight, vertexGrid[nextRow][nextCol]));
+                    tempList.add(new IntegerPair(vertexGrid[nextRow][nextCol], weight));
                 }
 
                 adjList.add(tempList);
             }
         }
 
-        // Modified Dijkstra's Algorithm
-        int size = m * n;
-        int sourceVertex = vertexGrid[0][0];
-        initSSSP(size, sourceVertex);
-
+        int ladderHeight = -1;
+        boolean[] visited = new boolean[m * n];
         PriorityQueue<IntegerPair> pq = new PriorityQueue<>();
-        pq.add(new IntegerPair(0, sourceVertex));
+        int source = vertexGrid[0][0], dest = vertexGrid[m - 1][n - 1];
+
+        // init
+        pq.add(new IntegerPair(source, 0));
+
         while (!pq.isEmpty()) {
-            IntegerPair temp = pq.poll();
-            int d = temp.first(), u = temp.second();
-            if (d == D.get(u)) {
-                for (IntegerPair v : adjList.get(u)) {
-                    int w_u_u2 = v.first(), u2 = v.second();
-                    if (D.get(u2) > D.get(u) + w_u_u2) {
-                        D.set(u2, D.get(u) + w_u_u2);
-                        p.set(u2, u);
-                        pq.add(new IntegerPair(D.get(u2), u2));
-                    }
-                }
-            }
+            IntegerPair v = pq.poll();
+            ladderHeight = v.second();
+            // System.out.println(v.first());
+            // System.out.println(ladderHeight);
+
+            // DFS results in Memory Limit Exceeded (MLE)
+            BFS(v.first(), dest, visited, adjList, ladderHeight, pq);
+
+            // System.out.println(Arrays.toString(visited).replace("[", "").replace("]", "").replace("false", ".").replace("true", "T").replace(",", " "));
+            // System.out.println();
+
+            if (visited[dest]) break;
         }
 
-        int res = -1;
-        int u = vertexGrid[m - 1][n - 1];
-        System.out.println(D.get(u));
-        while (p.get(u) != -1) {
-            System.out.printf(" %d", u);
-            int tempRes = D.get(u) - D.get(p.get(u));
-            if (tempRes > res) res = tempRes;
-            u = p.get(u);
-        }
-        System.out.println();
-        // System.out.println(Arrays.toString(p.toArray()));
-        out.write(Integer.toString(res).getBytes());
+        out.write(Integer.toString(ladderHeight).getBytes());
         br.close();
         out.close();
     }
 
-    public static void backtrack(int s, int u) {
-      if (u == -1 || u == s) {
-        System.out.printf("%d", u);
-        return;
-      }
-      backtrack(s, p.get(u));
-      System.out.printf(" %d", u);
+    public static void DFS(int source, int dest, boolean[] visited,
+                           ArrayList<ArrayList<IntegerPair>> adjList,
+                           int heightToExplore,
+                           PriorityQueue<IntegerPair> vertexToExplore) {
+        visited[source] = true;
+        if (source == dest) return;
+
+        for (IntegerPair v : adjList.get(source)) {
+            int val = v.first();
+            boolean isValid = heightToExplore >= v.second();
+
+            if (!isValid && !visited[val]) {
+                vertexToExplore.add(new IntegerPair(source, v.second()));
+                continue;
+            }
+
+            if (!visited[val]) {
+                DFS(val, dest, visited, adjList, heightToExplore, vertexToExplore);
+            }
+        }
+    }
+
+    public static void BFS(int source, int dest, boolean[] visited,
+                           ArrayList<ArrayList<IntegerPair>> adjList,
+                           int heightToExplore,
+                           PriorityQueue<IntegerPair> vertexToExplore) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        visited[source] = true;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+
+            if (u == dest) return;
+
+            for (IntegerPair v : adjList.get(u)) {
+                int val = v.first();
+                boolean isValid = heightToExplore >= v.second();
+
+                if (!isValid && !visited[val]) {
+                    vertexToExplore.add(new IntegerPair(u, v.second()));
+                    continue;
+                }
+
+                if (!visited[val]) {
+                    visited[val] = true;
+                    q.add(val);
+                }
+            }
+        }
     }
 }
 
@@ -121,9 +143,9 @@ class IntegerPair implements Comparable<IntegerPair> {
         this.second = second;
     }
 
-    // put weight in first position, sort by weight
+    // sort by weight
     public int compareTo(IntegerPair o) {
-        return this.first() - o.first();
+        return this.second() - o.second();
     }
 
     public Integer first() { return first; }
